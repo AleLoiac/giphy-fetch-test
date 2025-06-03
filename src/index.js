6fd02d0f84110d66a24ac82d2beb636e9254c49f
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-06-03 21:18:49 +0200

    Add button to fetch new gif

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import "./modern-normalize.css";
 import "./styles.css";
 
+const newGifBtn = document.querySelector("button");
 const img = document.querySelector("img");
-fetch(
-  "https://api.giphy.com/v1/gifs/translate?api_key=SPo3JCK4o2TrwOEWd89phqwnPcyZpJtg&s=shiba",
-  {
-    mode: "cors",
-  },
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (response) {
-    img.src = response.data.images.original.url;
-  });
+
+function fetchShibaGif() {
+  fetch(
+    "https://api.giphy.com/v1/gifs/translate?api_key=SPo3JCK4o2TrwOEWd89phqwnPcyZpJtg&s=shiba",
+    {
+      mode: "cors",
+    },
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      img.src = response.data.images.original.url;
+    });
+}
+
+newGifBtn.addEventListener("click", fetchShibaGif);
+
+fetchShibaGif();
