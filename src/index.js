d2b9d792fb15e1fe3500d900649d9db178a3a8d8
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-06-03 19:57:26 +0200

    Fetch gifs from giphy API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import "./styles.css";
+
+const img = document.querySelector("img");
+fetch(
+  "https://api.giphy.com/v1/gifs/translate?api_key=SPo3JCK4o2TrwOEWd89phqwnPcyZpJtg&s=shiba",
+  {
+    mode: "cors",
+  },
+)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (response) {
+    img.src = response.data.images.original.url;
+  });
