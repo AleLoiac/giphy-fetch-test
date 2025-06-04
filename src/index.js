48ca1311ca0d05a2fbc01d28b0f4376c9d97cc9c
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-06-04 13:59:08 +0200

    Add search box and load a new gif on input

--- src/index.js
+++ src/index.js
@@ -1,24 +1,42 @@
 import "./modern-normalize.css";
 import "./styles.css";
 
-const newGifBtn = document.querySelector("button");
+const newGifBtn = document.querySelector(".new-gif-btn");
+const searchBtn = document.querySelector(".search-btn");
+const input = document.querySelector("input");
 const img = document.querySelector("img");
+let currentTerm = "shiba";
 
-function fetchShibaGif() {
-  fetch(
-    "https://api.giphy.com/v1/gifs/translate?api_key=SPo3JCK4o2TrwOEWd89phqwnPcyZpJtg&s=shiba",
-    {
-      mode: "cors",
-    },
-  )
+function fetchGif(subject) {
+  const url = `https://api.giphy.com/v1/gifs/translate?api_key=SPo3JCK4o2TrwOEWd89phqwnPcyZpJtg&s=${subject}`;
+
+  fetch(url, {
+    mode: "cors",
+  })
     .then(function (response) {
       return response.json();
     })
     .then(function (response) {
+      if (!response.data || !response.data.images) {
+        alert("No gif found");
+        return;
+      }
       img.src = response.data.images.original.url;
+    })
+    .catch(function (err) {
+      console.log(err);
     });
 }
 
-newGifBtn.addEventListener("click", fetchShibaGif);
+newGifBtn.addEventListener("click", () => {
+  fetchGif(currentTerm);
+});
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
 
-fetchShibaGif();
+  const inputValue = input.value;
+  currentTerm = inputValue;
+  fetchGif(inputValue);
+});
+
+fetchGif(currentTerm);
